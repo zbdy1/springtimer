b4fd833ab67b57adc7d57a49db37073892629098
zbdy1 <cxg_401@163.com>
2017-08-24 10:52:33 +0800

    增加对广告的浏览次数增加

--- src/com/springtimer/ZengJiaCiShuTimer.java
+++ src/com/springtimer/ZengJiaCiShuTimer.java
@@ -13,24 +13,60 @@ import com.dbconfig.DBConnTools;
 import com.dbconfig.DBProperties;
 import com.pojo.TouTiaoBean;
 import com.util.Common;
-@Component  //import org.springframework.stereotype.Component;  
-public class ZengJiaCiShuTimer implements SpringTimerInterface{
-	
-	@Scheduled(cron="0 0 7-23 * * ? ")   //每小时执行一次
+
+@Component
+// import org.springframework.stereotype.Component;
+public class ZengJiaCiShuTimer implements SpringTimerInterface {
+
+	@Scheduled(cron = "0 0 7-23 * * ? ")
+	// 每小时执行一次
 	@Override
 	public void run() {
-		Map<String,Object> dbProperties=DBProperties.dbIntconfig;
+		Map<String, Object> dbProperties = DBProperties.dbIntconfig;
 		DBConnTools.connMysqlDB(dbProperties);
-		Calendar calendar=Calendar.getInstance();
+		Calendar calendar = Calendar.getInstance();
 		calendar.add(Calendar.DAY_OF_YEAR, -2);
-		String dateStart=Common.getDateString(calendar.getTime(), "yyyy-MM-dd");
-		String updateSql="update my_information set hit=hit+addfit(5,10) where FROM_UNIXTIME(begintime, '%Y-%m-%d' )>='"+dateStart+"'";
+		String dateStart = Common.getDateString(calendar.getTime(),
+				"yyyy-MM-dd");
+		String updateSql = "update my_information set hit=hit+addfit(5,10) where FROM_UNIXTIME(begintime, '%Y-%m-%d' )>='"
+				+ dateStart + "'";
 		System.out.println(updateSql);
 		try {
 			DBConnTools.myConn.createStatement().executeUpdate(updateSql);
+			String focusIds=getFocusIds();
+			if(focusIds!=null){
+				String updateSql2="update my_information set hit=hit+addfit(5,10) where id in ("+focusIds+")";
+				System.out.println(updateSql2);
+				DBConnTools.myConn.createStatement().executeUpdate(updateSql2);
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public String getFocusIds() {
+		String query = "select url from my_focus";
+		try {
+			ResultSet rs = DBConnTools.myConn.createStatement().executeQuery(
+					query);
+			StringBuffer resultIds = new StringBuffer();
+			while (rs.next()) {
+				String url = rs.getString("url");
+				if(url.startsWith("http://www.dlxc6.com/m/index.php?mod=information")){
+					String id = url.substring(url.lastIndexOf("=") + 1);
+					resultIds.append(id + ",");
+				}
+			}
+			if (resultIds.length() > 0) {
+				return resultIds.substring(0, resultIds.length() - 1);
+			} else {
+				return null;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
